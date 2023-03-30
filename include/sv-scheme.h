cc33eb9f69e2fa716a098bbc3f0b25791fce37c4
Ernesto Casablanca <casablancaernesto@gmail.com>
2023-03-31 00:40:44 +0200

    fix: extract feat: add delegation

--- include/sv-scheme.h
+++ include/sv-scheme.h
@@ -1,24 +1,27 @@
 #ifndef SV_SCHEME_H
 #define SV_SCHEME_H
 
+#include <string.h>
 #include <pbc/pbc.h>
 #include <nettle/sha1.h>
 #include "lib-misc.h"
+#include "shared.h"
 
-struct sv_init_params_struct
-{
-};
-typedef struct sv_init_params_struct *sv_init_params_ptr;
-typedef struct sv_init_params_struct sv_init_params_t[1];
+#define IDENTITY_SIZE 32
+#define WARRANT_SIZE 64
+
+typedef uint8_t sv_identity_t[IDENTITY_SIZE];
+typedef uint8_t serialized_warrant_t[WARRANT_SIZE];
 
 struct sv_public_params_struct
 {
-    pairing_t pairing; // The pairing used by the scheme.
-    element_t pk;      // The public key.
-    element_t P;       // The generator of G1.
-    int l1;            // Number of bits of an element in G1.
-    int l2;            // Number of bits of an element in G2.
-    int q;             // Order of the group.
+    pairing_t pairing;     // The pairing used by the scheme.
+    element_t pk;          // The public key.
+    element_t P;           // The generator of G1.
+    int l1;                // Number of bits of an element in G1.
+    int l2;                // Number of bits of an element in G2.
+    int q;                 // Order of the group.
+    hash_type_t hash_type; // Hash algorithm used by the scheme.
 };
 typedef struct sv_public_params_struct *sv_public_params_ptr;
 typedef struct sv_public_params_struct sv_public_params_t[1];
@@ -26,32 +29,27 @@ typedef struct sv_public_params_struct sv_public_params_t[1];
 struct sv_secret_params_struct
 {
     sv_public_params_ptr public_params; // Public parameters of the scheme.
-    element_t sk;                       // The master secret key.
+    element_t msk;                      // The master secret key.
 };
 typedef struct sv_secret_params_struct *sv_secret_params_ptr;
 typedef struct sv_secret_params_struct sv_secret_params_t[1];
 
-typedef struct
+struct warrant_struct
 {
+    sv_identity_t from;
+    sv_identity_t to;
+};
+typedef struct warrant_struct *warrant_ptr;
+typedef struct warrant_struct warrant_t[1];
 
-} identity_t;
-
-typedef struct
+struct delegation_struct
 {
-} warrant_t;
-
-typedef struct
-{
-} public_key_t;
-
-typedef struct
-{
-} secret_key_t;
-
-typedef struct
-{
-
-} delegation_t;
+    warrant_ptr m;
+    element_t r;
+    element_t S;
+};
+typedef struct delegation_struct *delegation_ptr;
+typedef struct delegation_struct delegation_t[1];
 
 typedef struct
 {
@@ -64,6 +62,37 @@ typedef struct
 } proxy_signature_t;
 
 /**
+ * @brief Serialize a warrant structure converting it into a byte array.
+ * This allows for easy hashing and storage of the warrant.
+ *
+ * @param buffer buffer to store the serialized warrant. It must be at least IDENTITY_SIZE * 2 bytes long.
+ * @param m warrant structure to be serialized.
+ * @return unsigned short size of the serialized warrant.
+ */
+unsigned short serialize_warrant(uint8_t buffer[WARRANT_SIZE], const warrant_t m);
+
+/**
+ * @brief Deserialize a warrant structure converting it from a byte array.
+ * This allows for easy hashing and storage of the warrant.
+ *
+ * @param buffer buffer to store the serialized warrant.
+ * @param m warrant structure to be serialized.
+ * @return unsigned short size of the serialized warrant.
+ */
+unsigned short deserialize_warrant(warrant_t m, const uint8_t buffer[WARRANT_SIZE]);
+
+/**
+ * @brief hash both the warrant and the random value r and return the result in h, an element of Zq*.
+ * @warning h must be initialized before calling this function.
+ *
+ * @param h element of Zq* where the hash is stored.
+ * @param w delegation which contains the value r that will be hashed.
+ * @param m warrant to be hashed.
+ * @param hash_type hash algorithm to be used.
+ */
+void hash_warrant_and_r(element_t h, delegation_t w, warrant_t m, hash_type_t hash_type);
+
+/**
  * @brief Initialization function for the scheme.
  * It takes as input a security parameter lambda and return a master key sk
  * and the system's parameters (G1, G2, H0, H1, H2, F1, F2, e, P, pk, q, l1, l2).
@@ -71,8 +100,9 @@ typedef struct
  * @param public_p All the public parameters created through the setup.
  * @param secret_p secret parameters created through the setup.
  * @param sec_lvl The security level the scheme is expected to have.
+ * @param hash_type The hash function used by the scheme.
  */
-void setup(sv_public_params_t public_p, sv_secret_params_t secret_p, int sec_lvl);
+void setup(sv_public_params_t public_p, sv_secret_params_t secret_p, int sec_lvl, hash_type_t hash_type);
 
 /**
  * @brief Produces the public key pk_id from an identity.
@@ -81,10 +111,9 @@ void setup(sv_public_params_t public_p, sv_secret_params_t secret_p, int sec_lvl
  *
  * @param pk_id Public key created for the user from their identifier.
  * @param public_p All the public parameters of the scheme.
- * @param identity Identity of the user.
- * @param len Length of the identity buffer.
+ * @param identity Identity of the user of IDENTITY_SIZE bytes.
  */
-void extract_p(element_t pk_id, sv_public_params_t public_p, const void *identity, int len);
+void extract_p(element_t pk_id, sv_public_params_t public_p, const sv_identity_t identity);
 
 /**
  * @brief Produces the secret key sk_id from an identity.
@@ -94,23 +123,24 @@ void extract_p(element_t pk_id, sv_public_params_t public_p, const void *identit
  *
  * @param pk_id Private key created for the user from their identifier.
  * @param secret_p All the secret parameters of the user.
- * @param identity Identity of the user.
- * @param len Length of the identity buffer.
+ * @param identity Identity of the user of IDENTITY_SIZE bytes.
  */
-void extract_s(element_t sk_id, sv_secret_params_t secret_p, const void *identity, int len);
+void extract_s(element_t sk_id, sv_secret_params_t secret_p, const sv_identity_t identity);
 
 /**
  * @brief Delegates the right to sign messages to the identity id.
  * The original signer selects k in Zq* and computes the following values:
- * - r = e(P, P)^k
- * - h = H1(m, r)
- * - S = h * d + k * P
+ * - r = e(P, P)^k (in GT)
+ * - h = H1(m, r) (in Zq*)
+ * - S = h * sk + k * P (in G1)
  * The resulting delegation is W = (m, r, S).
  *
- * @param sk_id secret key of the user who wants to delegate.
+ * @param w delegation to be created.
+ * @param sk secret key of the user.
  * @param m warrant to be delegated.
+ * @param public_p All the public parameters of the scheme.
  */
-void delegate(secret_key_t sk_id, warrant_t m);
+void delegate(delegation_t w, element_t sk, warrant_t m, sv_public_params_t public_p);
 
 /**
  * @brief Verifies the validity of a delegation.
@@ -122,10 +152,11 @@ void delegate(secret_key_t sk_id, warrant_t m);
  * In any other case, 0 is returned.
  *
  * @param w delegation to be verified.
- * @param id identity of the user who wants to delegate.
+ * @param identity identity of the user who wants to delegate.
+ * @param public_p All the public parameters of the scheme.
  * @return whether the delegation is valid (1) or not (0).
  */
-int del_verify(delegation_t w, identity_t id);
+int del_verify(delegation_t w, sv_identity_t identity, sv_public_params_t public_p);
 
 /**
  * @brief Produces a proxy signing key from a delegation.
