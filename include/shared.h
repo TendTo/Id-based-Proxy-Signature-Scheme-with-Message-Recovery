7d527a500fd616506da84e867d545d11e891a342
Ernesto Casablanca <casablancaernesto@gmail.com>
2023-03-31 00:39:55 +0200

    feat: add hash shared func

--- include/shared.h
+++ include/shared.h
@@ -3,7 +3,25 @@
 
 #include <assert.h>
 #include <pbc/pbc.h>
+#include <nettle/sha1.h>
+#include <nettle/sha2.h>
 
-void swap(void *a, void *b);
+typedef enum
+{
+    sha_1,
+    sha_256,
+    sha_512
+} hash_type_t;
 
-#endif
+/**
+ * @brief Generate the digest of the data using the hash_type algorithm
+ *
+ * @param digest ouput digest obtained by applying the hash_type algorithm to data
+ * @param data data to be hashed
+ * @param len length of ht data buffer in bytes
+ * @param hash_type hash algorithm to be used
+ * @return unsigned short length of the digest in bytes
+ */
+unsigned short hash(uint8_t **digest, const void *data, size_t len, hash_type_t hash_type);
+
+#endif // UTIL_H
--- src/shared.c
+++ src/shared.c
@@ -1,8 +1,31 @@
 #include "shared.h"
 
-void swap(void *a, void *b)
+unsigned short hash(uint8_t **digest, const void *data, size_t len, hash_type_t hash_type)
 {
-    void *temp = a;
-    a = b;
-    b = temp;
+    switch (hash_type)
+    {
+    case sha_1:
+        *digest = malloc(SHA1_DIGEST_SIZE);
+        struct sha1_ctx h;
+        sha1_init(&h);
+        sha1_update(&h, len, data);
+        sha1_digest(&h, SHA1_DIGEST_SIZE, *digest);
+        return SHA1_DIGEST_SIZE;
+    case sha_256:
+        *digest = malloc(SHA256_DIGEST_SIZE);
+        struct sha256_ctx h256;
+        sha256_init(&h256);
+        sha256_update(&h256, len, data);
+        sha256_digest(&h256, SHA256_DIGEST_SIZE, *digest);
+        return SHA256_DIGEST_SIZE;
+    case sha_512:
+        *digest = malloc(SHA512_DIGEST_SIZE);
+        struct sha512_ctx h512;
+        sha512_init(&h512);
+        sha512_update(&h512, len, data);
+        sha512_digest(&h512, SHA512_DIGEST_SIZE, *digest);
+        return SHA512_DIGEST_SIZE;
+    default:
+        return -1;
+    }
 }
