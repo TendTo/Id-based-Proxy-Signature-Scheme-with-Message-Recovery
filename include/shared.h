167d7f22029a7a4e08c5ac74664d6863da2bf6c3
Ernesto Casablanca <casablancaernesto@gmail.com>
2023-04-01 18:58:57 +0200

    feat: hash element func

--- include/shared.h
+++ include/shared.h
@@ -6,6 +6,8 @@
 #include <nettle/sha1.h>
 #include <nettle/sha2.h>
 
+#define MAX_DIGEST_SIZE 64
+
 typedef enum
 {
     sha_1,
@@ -22,6 +24,16 @@ typedef enum
  * @param hash_type hash algorithm to be used
  * @return unsigned short length of the digest in bytes
  */
-unsigned short hash(uint8_t **digest, const void *data, size_t len, hash_type_t hash_type);
+unsigned short hash(uint8_t digest[MAX_DIGEST_SIZE], const void *data, size_t len, hash_type_t hash_type);
+
+/**
+ * @brief Generate the digest of the element using the hash_type algorithm
+ * 
+ * @param digest output digest obtained by applying the hash_type algorithm to the element
+ * @param e element to be hashed
+ * @param hash_type hash algorithm to be used
+ * @return unsigned short length of the digest in bytes
+ */
+unsigned short hash_element(uint8_t digest[MAX_DIGEST_SIZE], element_t e, hash_type_t hash_type);
 
 #endif // UTIL_H
--- src/shared.c
+++ src/shared.c
@@ -1,31 +1,36 @@
 #include "shared.h"
 
-unsigned short hash(uint8_t **digest, const void *data, size_t len, hash_type_t hash_type)
+unsigned short hash(uint8_t digest[MAX_DIGEST_SIZE], const void *data, size_t len, hash_type_t hash_type)
 {
     switch (hash_type)
     {
     case sha_1:
-        *digest = malloc(SHA1_DIGEST_SIZE);
         struct sha1_ctx h;
         sha1_init(&h);
         sha1_update(&h, len, data);
-        sha1_digest(&h, SHA1_DIGEST_SIZE, *digest);
+        sha1_digest(&h, SHA1_DIGEST_SIZE, digest);
         return SHA1_DIGEST_SIZE;
     case sha_256:
-        *digest = malloc(SHA256_DIGEST_SIZE);
         struct sha256_ctx h256;
         sha256_init(&h256);
         sha256_update(&h256, len, data);
-        sha256_digest(&h256, SHA256_DIGEST_SIZE, *digest);
+        sha256_digest(&h256, SHA256_DIGEST_SIZE, digest);
         return SHA256_DIGEST_SIZE;
     case sha_512:
-        *digest = malloc(SHA512_DIGEST_SIZE);
         struct sha512_ctx h512;
         sha512_init(&h512);
         sha512_update(&h512, len, data);
-        sha512_digest(&h512, SHA512_DIGEST_SIZE, *digest);
+        sha512_digest(&h512, SHA512_DIGEST_SIZE, digest);
         return SHA512_DIGEST_SIZE;
     default:
         return -1;
     }
+}
+
+unsigned short hash_element(uint8_t digest[MAX_DIGEST_SIZE], element_t e, hash_type_t hash_type)
+{
+    int len = element_length_in_bytes(e);
+    uint8_t element_buffer[len];
+    element_to_bytes(element_buffer, e);
+    return hash(digest, element_buffer, len, hash_type);
 }
