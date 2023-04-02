9c4e062287325a21ccbb1abbc1342732548a6879
Ernesto Casablanca <casablancaernesto@gmail.com>
2023-04-03 00:23:54 +0200

    refactor: move shared functions into shared file

--- include/sv-scheme.h
+++ include/sv-scheme.h
@@ -4,52 +4,7 @@
 #include <string.h>
 #include <pbc/pbc.h>
 #include <nettle/sha1.h>
-#include "lib-misc.h"
 #include "shared.h"
-
-#define IDENTITY_SIZE 32
-#define WARRANT_SIZE 64
-
-typedef uint8_t sv_identity_t[IDENTITY_SIZE];
-typedef uint8_t serialized_warrant_t[WARRANT_SIZE];
-
-struct sv_public_params_struct
-{
-    pairing_t pairing;     // The pairing used by the scheme.
-    element_t pk;          // The public key.
-    element_t P;           // The generator of G1.
-    int l1;                // Half of number bits of an element in G1.
-    int l2;                // Half of number bits of an element in G1.
-    int q;                 // Order of the group.
-    hash_type_t hash_type; // Hash algorithm used by the scheme.
-};
-typedef struct sv_public_params_struct *sv_public_params_ptr;
-typedef struct sv_public_params_struct sv_public_params_t[1];
-
-struct sv_secret_params_struct
-{
-    sv_public_params_ptr public_params; // Public parameters of the scheme.
-    element_t msk;                      // The master secret key.
-};
-typedef struct sv_secret_params_struct *sv_secret_params_ptr;
-typedef struct sv_secret_params_struct sv_secret_params_t[1];
-
-struct warrant_struct
-{
-    sv_identity_t from; // Identity of the user that is delegating the signature.
-    sv_identity_t to;   // Identity of the user that is invested with the signature.
-};
-typedef struct warrant_struct *warrant_ptr;
-typedef struct warrant_struct warrant_t[1];
-
-struct delegation_struct
-{
-    warrant_ptr m; // Warrant of the delegation.
-    element_t r;   // r value used to verify the delegation (in GT).
-    element_t S;   // S value used to verify the delegation (in G1).
-};
-typedef struct delegation_struct *delegation_ptr;
-typedef struct delegation_struct delegation_t[1];
 
 struct proxy_signature_struct
 {
@@ -60,115 +15,6 @@ struct proxy_signature_struct
 };
 typedef struct proxy_signature_struct *proxy_signature_ptr;
 typedef struct proxy_signature_struct proxy_signature_t[1];
-
-/**
- * @brief Serialize a warrant structure converting it into a byte array.
- * This allows for easy hashing and storage of the warrant.
- *
- * @param buffer buffer to store the serialized warrant. It must be at least IDENTITY_SIZE * 2 bytes long.
- * @param m warrant structure to be serialized.
- * @return unsigned short size of the serialized warrant.
- */
-unsigned short serialize_warrant(uint8_t buffer[WARRANT_SIZE], const warrant_t m);
-
-/**
- * @brief Deserialize a warrant structure converting it from a byte array.
- * This allows for easy hashing and storage of the warrant.
- *
- * @param buffer buffer to store the serialized warrant.
- * @param m warrant structure to be serialized.
- * @return unsigned short size of the serialized warrant.
- */
-unsigned short deserialize_warrant(warrant_t m, const uint8_t buffer[WARRANT_SIZE]);
-
-/**
- * @brief hash both the warrant and the random value r and return the result in h, an element of Zq*.
- * @warning h must be initialized before calling this function.
- *
- * @param h element of Zq* where the hash is stored.
- * @param r element r that will be hashed.
- * @param m warrant to be hashed.
- * @param hash_type hash algorithm to be used.
- */
-void hash_warrant_and_r(element_t h, element_t r, warrant_t m, hash_type_t hash_type);
-
-/**
- * @brief Calculate the value of beta.
- * Starting from the message to be signed and the public parameters of the scheme, beta is obtained as:
- * beta = F1(msg) || F2(F1(msg)) (+) msg)
- *
- * @param beta output of the hash function.
- * @param msg message to be hashed.
- * @param msg_size size of the message to be signed in bytes.
- * @param public_p public parameters of the scheme.
- */
-void calculate_beta(uint8_t beta[], const uint8_t msg[], size_t msg_size, sv_public_params_t public_p);
-
-/**
- * @brief Initialization function for the scheme.
- * It takes as input a security parameter lambda and return a master key sk
- * and the system's parameters (G1, G2, H0, H1, H2, F1, F2, e, P, pk, q, l1, l2).
- *
- * @param public_p All the public parameters created through the setup.
- * @param secret_p secret parameters created through the setup.
- * @param sec_lvl The security level the scheme is expected to have.
- * @param hash_type The hash function used by the scheme.
- */
-void setup(sv_public_params_t public_p, sv_secret_params_t secret_p, int sec_lvl, hash_type_t hash_type);
-
-/**
- * @brief Produces the public key pk_id from an identity.
- * It uses the hash function H0 to map the any string {0, 1}*, representing the identity,
- * to an element of G1: pk_id = H0(identity).
- *
- * @param pk_id Public key created for the user from their identifier.
- * @param identity Identity of the user of IDENTITY_SIZE bytes.
- * @param public_p All the public parameters of the scheme.
- */
-void extract_p(element_t pk_id, const sv_identity_t identity, sv_public_params_t public_p);
-
-/**
- * @brief Produces the secret key sk_id from an identity.
- * It uses the hash function H0 to map the any string {0, 1}*, representing the identity,
- * to an element of G1.
- * Then multiplies the master key sk with the result of the hash function: sk_id = sk * H0(identity).
- *
- * @param pk_id Private key created for the user from their identifier.
- * @param identity Identity of the user of IDENTITY_SIZE bytes.
- * @param secret_p All the secret parameters of the user.
- */
-void extract_s(element_t sk_id, const sv_identity_t identity, sv_secret_params_t secret_p);
-
-/**
- * @brief Delegates the right to sign messages to the identity id.
- * The original signer selects k in Zq* and computes the following values:
- * - r = e(P, P)^k (in GT)
- * - h = H1(m, r) (in Zq*)
- * - S = h * sk + k * P (in G1)
- * The resulting delegation is W = (m, r, S).
- *
- * @param w delegation to be created.
- * @param sk secret key of the user.
- * @param m warrant to be delegated.
- * @param public_p All the public parameters of the scheme.
- */
-void delegate(delegation_t w, element_t sk, warrant_t m, sv_public_params_t public_p);
-
-/**
- * @brief Verifies the validity of a delegation.
- * After receiving the delegation W = (m, r, S), the verifier computes the following values:
- * - h = H1(m, r)
- * - q = H0(identity)
- * The delegation is valid and 1 is returned if and only if the following equation holds:
- * e(S, P) = e(pk_id, P)^h * r.
- * In any other case, 0 is returned.
- *
- * @param w delegation to be verified.
- * @param identity identity of the user who supposedly created the delegation.
- * @param public_p All the public parameters of the scheme.
- * @return whether the delegation has been created by the `identity` user (1) or not (0).
- */
-int del_verify(delegation_t w, sv_identity_t identity, sv_public_params_t public_p);
 
 /**
  * @brief Produces a proxy signing key from a delegation.
@@ -211,30 +57,6 @@ void p_sign(proxy_signature_t p_sig, element_t k_sign, delegation_t w, const uin
  * @param public_p All the public parameters of the scheme.
  */
 unsigned short sign_verify(proxy_signature_t p_sig, sv_public_params_t public_p);
-
-/**
- * @brief Clear the public param struct.
- * Makes sure all elements are cleared.
- *
- * @param public_p Parameters public publicly.
- */
-void public_param_clear(sv_public_params_t public_p);
-
-/**
- * @brief Clear the secret param struct.
- * Make sure all elements are cleared.
- *
- * @param secret_p Parameters known only by the original signer.
- */
-void secret_param_clear(sv_secret_params_t secret_p);
-
-/**
- * @brief Clear the warrant struct.
- * Make sure all elements are cleared.
- *
- * @param w Warrant to be cleared.
- */
-void delegation_clear(delegation_t w);
 
 /**
  * @brief Clear the proxy signature struct.
--- src/shared.c
+++ src/shared.c
@@ -1,5 +1,20 @@
 #include "shared.h"
 
+unsigned int non_generic_dlog_secure_size_by_security_level(unsigned int level)
+{
+    /* NIST (2020) */
+    if (level <= 80)
+        return 1024;
+    else if (level <= 112)
+        return 2048;
+    else if (level <= 128)
+        return 3072;
+    else if (level <= 192)
+        return 7680;
+    else
+        return 15360;
+}
+
 unsigned short hash(uint8_t digest[MAX_DIGEST_SIZE], const void *data, size_t len, hash_type_t hash_type)
 {
     switch (hash_type)
@@ -33,4 +48,182 @@ unsigned short hash_element(uint8_t digest[MAX_DIGEST_SIZE], element_t e, hash_t
     uint8_t element_buffer[len];
     element_to_bytes(element_buffer, e);
     return hash(digest, element_buffer, len, hash_type);
+}
+
+unsigned short serialize_warrant(uint8_t buffer[WARRANT_SIZE], const warrant_t m)
+{
+    memcpy(buffer, m->from, IDENTITY_SIZE);
+    memcpy(buffer + IDENTITY_SIZE, m->to, IDENTITY_SIZE);
+    return WARRANT_SIZE;
+}
+
+unsigned short deserialize_warrant(warrant_t m, const uint8_t buffer[WARRANT_SIZE])
+{
+    memcpy(m->from, buffer, IDENTITY_SIZE);
+    memcpy(m->to, buffer + IDENTITY_SIZE, IDENTITY_SIZE);
+    return WARRANT_SIZE;
+}
+
+void hash_warrant_and_r(element_t h, element_t r, warrant_t m, hash_type_t hash_type)
+{
+    int size = element_length_in_bytes(r);
+    uint8_t digest[MAX_DIGEST_SIZE];
+    uint8_t h_buffer[WARRANT_SIZE + size];
+    element_to_bytes(h_buffer + WARRANT_SIZE, r);
+    serialize_warrant(h_buffer, m);
+    unsigned short digest_size = hash(digest, h_buffer, WARRANT_SIZE + size, hash_type);
+    element_from_hash(h, digest, digest_size);
+}
+
+void calculate_beta(uint8_t beta[], const uint8_t raw_msg[], size_t msg_size, sv_public_params_t public_p)
+{
+    // make sure the msg length is l2 and the last bytes are 0
+    uint8_t msg[public_p->l2];
+    if (msg_size >= public_p->l2)
+        msg_size = public_p->l2;
+    else
+        memset(msg, 0, public_p->l2);
+    memcpy(msg, raw_msg, msg_size);
+
+    // beta_left = F1(msg)
+    uint8_t beta_left[MAX_DIGEST_SIZE];
+    hash(beta_left, msg, public_p->l2, public_p->hash_type);
+
+    // beta_right = F2(F1(msg)) (+) msg
+    uint8_t beta_right[MAX_DIGEST_SIZE];
+    hash(beta_right, beta_left, public_p->l1, public_p->hash_type);
+    for (int i = 0; i < public_p->l2; i++)
+        beta_right[i] = beta_right[i] ^ msg[i];
+
+    // beta = beta_left || beta_right
+    memcpy(beta, beta_left, public_p->l1);
+    memcpy(beta + public_p->l1, beta_right, public_p->l2);
+}
+
+void setup(sv_public_params_t public_p, sv_secret_params_t secret_p, int lambda, hash_type_t hash_type)
+{
+    pbc_param_t pairing_p;
+    pbc_param_init_a_gen(pairing_p, generic_dlog_secure_size_by_security_level(lambda), non_generic_dlog_secure_size_by_security_level(lambda) / 2);
+    pairing_init_pbc_param(public_p->pairing, pairing_p);
+    pbc_param_clear(pairing_p);
+
+    // secret params init
+    element_init_Zr(secret_p->msk, public_p->pairing);
+
+    // secret params setup
+    element_random(secret_p->msk);
+    secret_p->public_params = public_p;
+
+    // public params init
+    element_init_G1(public_p->P, public_p->pairing);
+    element_init_G1(public_p->pk, public_p->pairing);
+
+    // public params setup
+    element_random(public_p->P);
+    element_mul_zn(public_p->pk, public_p->P, secret_p->msk);
+    public_p->q = pairing_length_in_bytes_x_only_G1(public_p->pairing);
+    public_p->l1 = public_p->q / 2;
+    public_p->l2 = public_p->q - public_p->l1;
+    public_p->hash_type = hash_type;
+}
+
+void extract_p(element_t pk_id, const sv_identity_t identity, sv_public_params_t public_p)
+{
+    // Hashing
+    uint8_t digest[MAX_DIGEST_SIZE];
+    unsigned short digest_len = hash(digest, identity, IDENTITY_SIZE, public_p->hash_type);
+
+    // Generating pk for the user
+    element_init_G1(pk_id, public_p->pairing);
+    element_from_hash(pk_id, digest, digest_len);
+}
+
+void extract_s(element_t sk_id, const sv_identity_t identity, sv_secret_params_t secret_p)
+{
+    // Hashing
+    uint8_t digest[MAX_DIGEST_SIZE];
+    unsigned short digest_len = hash(digest, identity, IDENTITY_SIZE, secret_p->public_params->hash_type);
+
+    // Generating sk for the user
+    element_init_G1(sk_id, secret_p->public_params->pairing);
+    element_from_hash(sk_id, digest, digest_len);
+    element_mul_zn(sk_id, sk_id, secret_p->msk);
+}
+
+void delegate(delegation_t w, element_t sk, warrant_t m, sv_public_params_t public_p)
+{
+    w->m = m;
+
+    element_t k, h, temp;
+    element_init_Zr(k, public_p->pairing);
+    element_init_Zr(h, public_p->pairing);
+    element_init_GT(w->r, public_p->pairing);
+    element_init_G1(w->S, public_p->pairing);
+    element_init_G1(temp, public_p->pairing);
+
+    // k <- Zr (random in Zr)
+    element_random(k);
+
+    // r = e(P, P)^k (in GT)
+    element_pairing(w->r, public_p->P, public_p->P);
+    element_pow_zn(w->r, w->r, k);
+
+    // h = H(m, ra) (in Zr)
+    hash_warrant_and_r(h, w->r, m, public_p->hash_type);
+
+    // S = h * sk + k * P (in G1)
+    element_mul_zn(w->S, sk, h);
+    element_mul_zn(temp, public_p->P, k);
+    element_add(w->S, w->S, temp);
+
+    element_clear(k);
+    element_clear(h);
+    element_clear(temp);
+}
+
+int del_verify(delegation_t w, sv_identity_t identity, sv_public_params_t public_p)
+{
+    element_t h, pk, left_el, right_el;
+    element_init_Zr(h, public_p->pairing);
+    element_init_G1(pk, public_p->pairing);
+    element_init_GT(left_el, public_p->pairing);
+    element_init_GT(right_el, public_p->pairing);
+
+    hash_warrant_and_r(h, w->r, w->m, public_p->hash_type);
+
+    extract_p(pk, identity, public_p);
+
+    // e(S, P) = e(Pub, pk)^h * r
+    element_pairing(left_el, w->S, public_p->P);
+
+    element_pairing(right_el, pk, public_p->pk);
+    element_pow_zn(right_el, right_el, h);
+    element_mul(right_el, right_el, w->r);
+
+    int res = element_cmp(left_el, right_el);
+
+    element_clear(h);
+    element_clear(pk);
+    element_clear(left_el);
+    element_clear(right_el);
+
+    return res == 0;
+}
+
+void public_param_clear(sv_public_params_t public_p)
+{
+    element_clear(public_p->P);
+    element_clear(public_p->pk);
+    pairing_clear(public_p->pairing);
+}
+
+void secret_param_clear(sv_secret_params_t secret_p)
+{
+    element_clear(secret_p->msk);
+}
+
+void delegation_clear(delegation_t w)
+{
+    element_clear(w->r);
+    element_clear(w->S);
 }
