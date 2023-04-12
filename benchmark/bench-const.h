22099ea1f8d3924fa94af0bb7ac898b359bfb41f
Ernesto Casablanca <casablancaernesto@gmail.com>
2023-04-12 23:01:45 +0200

    docs: update usage in bench

--- benchmark/bench-const.h
+++ benchmark/bench-const.h
@@ -8,7 +8,7 @@
 
 // Usage tooltip
 #define BENCH_USAGE \
-    "Usage: %s \n\
+    "Usage: %s [options] \n\
 Use -h to know more informations\n"
 
 // Help tooltip
@@ -17,13 +17,14 @@ Use -h to know more informations\n"
 " PROJECT_NAME " benchmarks, version " VERSION "\n\
 Usage: %s \n\
 " SEPARATOR "\
--h   -  shows the help tooltip\n\
--v   -  use verbose output\n\
--a   -  hash algorithm [sha1, sha256, sha512] (sha1)\n\
--l   -  security level (80)\n\
--t   -  maximum sampling time in seconds (4)\n\
--i   -  maximum number of samples (4000)\n\
--s   -  if set, use the seed for random functions\n"
+Options:\n\
+\t-h   -  shows the help tooltip\n\
+\t-v   -  use verbose output\n\
+\t-a   -  hash algorithm [sha1, sha256, sha512] (sha1)\n\
+\t-l   -  security level (80)\n\
+\t-t   -  maximum sampling time in seconds (4)\n\
+\t-i   -  maximum number of samples (4000)\n\
+\t-s   -  if set, use the seed for random functions\n"
 
 struct bench_param_struct
 {
